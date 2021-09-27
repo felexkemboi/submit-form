4141d4bd447de96b20b0cae648a971dfe7001a3a
felexkemboi <felokemboi10@gmail.com>
2021-09-27 20:58:41 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Form />
+  </div>
+</template>
+
+<script>
+import Form from '@/components/Form.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Form,
+  },
+};
+</script>
